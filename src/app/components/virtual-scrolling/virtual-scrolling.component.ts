10bedecff5ee8f811b1438ef800f0ffcd6b402f1
user7d11 <user7w11@gmail.com>
2022-09-19 17:47:25 +0200

    install faker

--- src/app/components/virtual-scrolling/virtual-scrolling.component.ts
+++ src/app/components/virtual-scrolling/virtual-scrolling.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+// import * as faker from 'faker';
+import { faker } from '@faker-js/faker';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-virtual-scrolling',
   templateUrl: './virtual-scrolling.component.html',
   styleUrls: ['./virtual-scrolling.component.scss']
 })
-export class VirtualScrollingComponent implements OnInit {
+export class VirtualScrollingComponent {
 
-  constructor() { }
+  public fakeData: Observable<Person[]> = this._generatePersonData(30000);
 
-  ngOnInit(): void {
+  private _generatePersonData(count: number): Observable<Person[]> {
+    const array: Person[] = [];
+    for (let i = 0; i < count; i++) {
+      array.push({
+        name: faker.internet.userName()
+      })
+    }
+    return of(array);
   }
 
+}
+
+interface Person {
+  name: string;
 }
